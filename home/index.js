63062a578bc95b7e3ab78944fa14b21865e80d29
danielacasian <casiandaniela999@gmail.com>
2025-01-15 12:52:22 +0200

    button

---
+++ home/index.js
@@ -0,0 +1,23 @@
+document.querySelectorAll(".menu-button").forEach(button =>{
+    button.addEventListener('click', function(event) {
+        document.querySelectorAll('.menu-item').forEach(item =>{
+            if(item.querySelector('.menu-button')!==button){
+                 item.classList.remove('menu-item-open')
+            }
+           
+        })
+        event.isClick = true
+        button.parentElement.classList.toggle('menu-item-open')
+    } )
+})
+
+document.body.addEventListener('click', function(event) {
+    if (
+        event._isClik == true || 
+        event.target.classList.contains("menu-button") == true 
+    )return
+
+    document.querySelectorAll('.menu-item').forEach(item =>{
+        item.classList.remove('menu-item-open')
+    })
+})
